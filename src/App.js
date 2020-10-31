38ef151a18d349e67b2872b2d0c06ba5ba8cf745
wasif-1991 <67942918+wasif-1991@users.noreply.github.com>
2020-10-31 13:53:00 +0500

    Updated for indexing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Bootcamp 2020 from Wasif!</h1>
+      <h1>Hello Bootcamp 2020 from Wasif Ahmed!</h1>
     </div>
   );
 }
