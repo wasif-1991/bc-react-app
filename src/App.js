9aaa2c1ba9100af3ef145b5f345ffb6ad090deab
wasif-1991 <67942918+wasif-1991@users.noreply.github.com>
2020-10-31 13:17:38 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Reaact from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Bootcamp 2020 from Wasif!</h1>
+    </div>
+  );
+}
+
+export default App;
